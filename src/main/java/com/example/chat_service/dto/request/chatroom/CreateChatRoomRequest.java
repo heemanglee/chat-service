986522f4f94a699d068e4e90c795cbefab7e2a7f
heemanglee <wishhopelee@gmail.com>
2025-09-07 10:46:21 +0900

    feat: DTO에 @Schema 애너테이션 추가

--- src/main/java/com/example/chat_service/dto/request/chatroom/CreateChatRoomRequest.java
+++ src/main/java/com/example/chat_service/dto/request/chatroom/CreateChatRoomRequest.java
@@ -4,12 +4,16 @@ import com.example.chat_service.domain.RoomStatus;
 import com.example.chat_service.domain.RoomType;
 import com.example.chat_service.domain.Visibility;
 import com.example.chat_service.validator.common.ValidEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CreateChatRoomRequest {
 
+    @NotBlank
     @Size(max = 20)
     private String title;
 
@@ -20,14 +24,17 @@ public class CreateChatRoomRequest {
 
     @NotBlank
     @ValidEnum(enumClass = RoomStatus.class, ignoreCase = true)
+    @Schema(allowableValues = {"ACTIVE", "LEFT", "REMOVED"})
     private String status;
 
     @NotNull
     @ValidEnum(enumClass = RoomType.class, ignoreCase = true)
+    @Schema(allowableValues = {"CHANNEL", "PERSONAL"})
     private String type;
 
     @NotNull
     @ValidEnum(enumClass = Visibility.class, ignoreCase = true)
+    @Schema(allowableValues = {"PUBLIC", "PRIVATE"})
     private String visibility;
 
     @NotNull
